148b500a78915bfc9b908a5a90f408201250762f
Jericho <jericho.6367@gmail.com>
2021-02-14 02:55:02 +0900

    Allow all headers.

--- src/backend/public/index.php
+++ src/backend/public/index.php
@@ -2,7 +2,7 @@
 
 header('Access-Control-Allow-Origin: https://app.local');
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+header('Access-Control-Allow-Headers: *');
 
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
